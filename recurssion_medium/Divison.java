660c10a80c91f9c4dd9787aecec947034057f7e2
Harshit Shahdeo <harshitshahdeo99@gmail.com>
2025-05-25 15:48:14 +0530

    Update Divison.java

--- recurssion_medium/Divison.java
+++ recurssion_medium/Divison.java
@@ -15,9 +15,15 @@ public class Divison {
         System.out.println(" The quotient of the following division is " + result);
     }
     public static int divide(int dividend, int divisor){
+
+        // if we divide Integer.MIN_VALUE with -1 it will give us stack overflow error because it will result in 2,147,483,648, which exceeds Integer.MAX_VALUE
+        
         if(dividend == Integer.MIN_VALUE && divisor == -1){
             return Integer.MAX_VALUE;
         }
+
+        // This will determine the sign that the integer shpuld have in our result of divison 
+        
         int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
          long dividendL = Math.abs(dividend);
         long divisorL = Math.abs(divisor);
@@ -29,6 +35,10 @@ if(divisor > dividend){
 else return sign * result;
 
     }
+
+    // we will need a helper method to use recurssion by dividiing our dividend with big chunks of divisor while maintaining the number of multiples.
+    // we need to do this because if again we divide Integer.MIN_VALUE or MAX_VALUE by a number like 1, the recurssion will call itself the no. of times equal to the value of the dividend,
+    // which will result in memory running out as each recurssive calls has its own memory.
     private static int helper(long dividend, long divisor){
         long sum = divisor;
         int multiple = 1;
@@ -43,6 +53,7 @@ else return sign * result;
         }
         else{
             int result =  multiple + helper( (dividend -sum), divisor);
-        return result;}
+        return result;
+        }
     }
 }
